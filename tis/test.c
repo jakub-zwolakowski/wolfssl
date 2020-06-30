182b8a332655b493cb6045b025cc6e2366be8d3f
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2020-06-30 13:08:54 +0200

    Cosmetic change.

--- tis/test.c
+++ tis/test.c
@@ -1,16 +1,4 @@
 
-extern int test_KO; /* Used to cause non-determinism if test fails. */
-
-/* One macro to rule them all. */
-#define MAKE_TEST_BIS(NAME) \
-int NAME ## _test(void); \
-int main_ ## NAME ## _test(void) { \
-    if ( NAME ## _test() == 0) \
-        return 0; \
-    else \
-        return test_KO; \
-}
-
 #define MAKE_TEST(NAME) \
 int NAME ## _test(void); \
 int main_ ## NAME ## _test(void) { \
