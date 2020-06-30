37b00baea382d79ba7dabb61af449892765d14ba
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2020-06-30 10:55:35 +0200

    Changed test config.

--- tis/test.c
+++ tis/test.c
@@ -2,13 +2,19 @@
 extern int test_KO; /* Used to cause non-determinism if test fails. */
 
 /* One macro to rule them all. */
-#define MAKE_TEST(NAME) \
+#define MAKE_TEST_BIS(NAME) \
 int NAME ## _test(void); \
 int main_ ## NAME ## _test(void) { \
     if ( NAME ## _test() == 0) \
         return 0; \
     else \
         return test_KO; \
+}
+
+#define MAKE_TEST(NAME) \
+int NAME ## _test(void); \
+int main_ ## NAME ## _test(void) { \
+    return NAME ## _test(); \
 }
 
 MAKE_TEST(error)
