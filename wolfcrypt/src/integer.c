759b5663edd8f48a42764b58517b14a2ec5c75c9
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2020-12-03 14:08:36 +0100

    Revert "Fix UB#3"
    This reverts commit cb465a52ccfbc06b1c3641cfbd3c8da2dbab153a.

--- wolfcrypt/src/integer.c
+++ wolfcrypt/src/integer.c
@@ -3447,8 +3447,8 @@ int fast_s_mp_mul_digs (mp_int * a, mp_int * b, mp_int * c, int digs)
 
       /* execute loop */
       for (iz = 0; iz < iy; ++iz) {
-         _W += ((mp_word)*tmpx++)*((mp_word)*tmpy);
-         if(iz + 1 < iy) tmpy--;
+         _W += ((mp_word)*tmpx++)*((mp_word)*tmpy--);
+
       }
 
       /* store term */
